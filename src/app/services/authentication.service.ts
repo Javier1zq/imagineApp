29492c87cd3aac3adceb1d50bfbbd42cd6d737c1
Javier1zq <izquierdoj98@gmail.com>
2021-04-27 15:37:31 +0200

    Login done

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/member-delimiter-style */
+/* eslint-disable arrow-body-style */
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Injectable } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import { HttpClient } from '@angular/common/http';
+import { map, tap, switchMap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, from } from 'rxjs';
+const { Storage } = Plugins;
+const TOKEN_KEY = 'my-token';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  isAuthenticated: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null);
+  token = '';
+  constructor(private http: HttpClient) {
+    this.loadToken();
+  }
+
+  async loadToken() {
+    const token = await Storage.get({ key: TOKEN_KEY });
+    if (token && token.value) {
+      console.log('set token: ', token.value);
+      this.token = token.value;
+      this.isAuthenticated.next(true);
+    } else {
+      this.isAuthenticated.next(false);
+    }
+  }
+
+  login(credentials: {email, password}): Observable<any> {
+    return this.http.post(`https://reqres.in/api/login`, credentials).pipe(
+      map((data: any) => data.token),
+      switchMap(token => {
+        return from(Storage.set({key: TOKEN_KEY, value: token}));
+      }),
+      tap(_ => {
+        this.isAuthenticated.next(true);
+      })
+    );
+  }
+
+  logout(): Promise<void> {
+    this.isAuthenticated.next(false);
+    return Storage.remove({key: TOKEN_KEY});
+  }
+}
